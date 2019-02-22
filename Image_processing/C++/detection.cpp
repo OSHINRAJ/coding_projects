a37022454bbd77b34b37a9eeee82239d2831e4dd
OSHINRAJ K M <oshinraj1996champad@gmail.com>
2019-02-22 12:14:36 +0530

    Add files via upload

--- Image_processing/C++/detection.cpp
+++ Image_processing/C++/detection.cpp
@@ -20,7 +20,7 @@ int main(int argc, char *argv[])
 		cout<<"usage : ./imgProcess <operation> <image-file> <others..>"<<endl;
 		cout<<"/*****OPERATIONS**********/\n1:   copyImage()\n2:   inverseImage()\n";
 		cout<<"3:   edgeDetect()\n4:   brightnessCtrl()\n5:   contrastCtrl()\n/************************/\n";
-		cout<<"<operation> : 1 => <other..s>:output image file name\n";
+		cout<<"<operation> : 1 => <others..>:output image file name\n";
 		return -1;
 	}
 	int rate;
@@ -33,20 +33,20 @@ int main(int argc, char *argv[])
 
 	switch(operation) {
 		case 1:	img_in.readImage(argv[2]); 
-				img_out.copyImage(img_in);
+				img_out.copyImage(&img_in);
 				img_out.writeImage(argv[3]);
 				break;
 		case 2:	img_in.readImage(argv[2]);		
-				img_out.inverseImage(img_in);
+				img_out.inverseImage(&img_in);
 				img_out.writeImage(argv[2]);
 				break;
 		case 3:	img_in.readImage(argv[2]);
-				img_out.edgeDetect(img_in);
+				img_out.edgeDetect(&img_in);
 				img_out.writeImage(argv[2]);
 				break;
 		case 4:	rate = atoi(argv[3]);
 				img_in.readImage(argv[2]);
-				img_out.brightnessCtrl(img_in, rate);
+				img_out.brightnessCtrl(&img_in, rate);
 				img_out.writeImage(argv[2]);
 				break;
 /*		case 5:	img_in.readImage(argv[2]);
--- Image_processing/C++/image.h
+++ Image_processing/C++/image.h
@@ -13,26 +13,35 @@ class Image {
 	int height;
 	int width;
 	int maxPixelVal;
-	uint8_t Pixel[MAX_HEIGHT][MAX_WIDTH];
+	uint8_t **Pixel;
 
-	public: 
+	public:
 	void setupImage(unsigned char inPixelArray[]);
 	void readImage(char *file_name);
 	void writeImage(char *file_name);
 	int getHeight() {return height;}
 	int getWidth() {return width;}
 	int getMaxPixelVal() {return maxPixelVal;}
-	/*
-	   void setHeight(int h) {height = h;}
-	   void setWidth(int w) {width = w;}
-	   void setMaxPixelVal(int p) {maxPixelVal = p;}
-	   */
-	void copyImage(Image img);
-	void inverseImage(Image img);
-	void edgeDetect(Image img);
-	int contrastCtrl(Image img);
-	int brightnessCtrl(Image img, int rate);
+	void copyImage(Image *img);
+	void inverseImage(Image *img);
+	void edgeDetect(Image *img);
+	int contrastCtrl(Image *img);
+	int brightnessCtrl(Image *img, int rate);
 	void pixelWrite(char file_name[]);
+	void makePixelArray(){
+		
+		Pixel = new uint8_t*[height];
+		for(int i = 0; i < height; i++)
+			Pixel[i] = new uint8_t[width];
+	}
+
+	~Image() {
+
+		for(int i = 0; i < height; i++)
+			delete [] Pixel[i];
+		delete [] Pixel;	
+	}
+
 
 };
 
@@ -65,7 +74,9 @@ void Image::readImage(char file_name[])
 	   ss << inputLine;
 	   ss>>maxPixelVal;*/
 	inFile>>maxPixelVal;
-	inFile.read(reinterpret_cast<char *>(Pixel), height*width*sizeof(uint8_t));
+	makePixelArray();
+	for (i = 0; i < height; i++)
+		inFile.read(reinterpret_cast<char *>(Pixel[i]), width*sizeof(uint8_t));
 	inFile.close();
 }
 
@@ -84,34 +95,35 @@ void Image::writeImage(char file_name[])
 	outFile<<width<<' '<<height<<endl;
 	/* Maximum pixel value */
 	outFile<<maxPixelVal<<endl;
-
-	outFile.write(reinterpret_cast<char *>(Pixel), height*width*sizeof(uint8_t));
+	for (i = 0; i < height; i++)
+		outFile.write(reinterpret_cast<char *>(Pixel[i]), width*sizeof(uint8_t));
 	outFile.close();
 }
 
-void Image::edgeDetect(Image img)
+void Image::edgeDetect(Image *img)
 {
 	int i, j, x, y, x_val, y_val;
 	int x_mask[3][3]={{1,0,-1},{2,0,-2},{1,0,-1}};
 	int y_mask[3][3]={{1,2,1},{0,0,0},{-1,-2,-1}};
 	unsigned char ch;
 
-	strcpy(magic, img.magic);
-	height = img.height;
-	width = img.width;
-	maxPixelVal = img.maxPixelVal;
+	strcpy(magic, img->magic);
+	height = img->height;
+	width = img->width;
+	maxPixelVal = img->maxPixelVal;
+	makePixelArray();
 
 	for (i = 0; i < height; i++) {
 		for ( j = 0; j < width; j++) {
 			x_val = 0;
 			y_val = 0;
 			if ( i == 0 || j == 0 || i == (height - 1) || j == (width - 1))
-				Pixel[i][j] = img.Pixel[i][j];
+				Pixel[i][j] = img->Pixel[i][j];
 			else{
 				for (x = 0; x < 3; x++)
 					for (y = 0; y < 3; y++){
-						x_val += x_mask[x][y] * img.Pixel[i+x-1][j+y-1];
-						y_val += y_mask[x][y] * img.Pixel[i+x-1][j+y-1];
+						x_val += x_mask[x][y] * img->Pixel[i+x-1][j+y-1];
+						y_val += y_mask[x][y] * img->Pixel[i+x-1][j+y-1];
 					}
 				Pixel[i][j] = (uint8_t)sqrt((x_val * x_val) + (y_val * y_val));
 			}
@@ -119,62 +131,65 @@ void Image::edgeDetect(Image img)
 	}
 }
 
-void Image::inverseImage(Image img)
+void Image::inverseImage(Image *img)
 {
 	int i, j;
-	strcpy(magic, img.magic);
-	height = img.height;
-	width = img.width;
-	maxPixelVal = img.maxPixelVal;
+	strcpy(magic, img->magic);
+	height = img->height;
+	width = img->width;
+	maxPixelVal = img->maxPixelVal;
+	makePixelArray();
 
 	for (i = 0; i < height; i++) {
 		for ( j = 0; j < width; j++) {
-			Pixel[i][j] = ~img.Pixel[i][j];
+			Pixel[i][j] = ~img->Pixel[i][j];
 		}
 	}
 }
-void Image::copyImage(Image img)
+void Image::copyImage(Image *img)
 {
 	int i, j;
-	strcpy(magic, img.magic);
-	height = img.height;
-	width = img.width;
-	maxPixelVal = img.maxPixelVal;
-
+	strcpy(magic, img->magic);
+	height = img->height;
+	width = img->width;
+	maxPixelVal = img->maxPixelVal;
+	makePixelArray();
 	for (i = 0; i < height; i++) {
 		for ( j = 0; j < width; j++) {
-			Pixel[i][j] = img.Pixel[i][j];
+			Pixel[i][j] = img->Pixel[i][j];	
 		}
 	}
 }
-int Image::brightnessCtrl(Image img, int rate)
+int Image::brightnessCtrl(Image *img, int rate)
 {
 	if(rate < -100 || rate > 100)
 		return -1;
 	int i, j;
 	float rate_value;
-	strcpy(magic, img.magic);
-	height = img.height;
-	width = img.width;
-	rate_value = img.maxPixelVal/ (100 / rate);
-	maxPixelVal = img.maxPixelVal;
+	strcpy(magic, img->magic);
+	height = img->height;
+	width = img->width;
+	rate_value = img->maxPixelVal/ (100 / rate);
+	maxPixelVal = img->maxPixelVal;
+	makePixelArray();
+
 	for (i = 0; i < height; i++) {
 		for ( j = 0; j < width; j++) {
 			if(rate_value > 0)
-				if ((int)img.Pixel[i][j] + rate_value > maxPixelVal)
+				if ((int)img->Pixel[i][j] + rate_value > maxPixelVal)
 					Pixel[i][j] = maxPixelVal;
 				else
-					Pixel[i][j] = img.Pixel[i][j] + rate_value;
+					Pixel[i][j] = img->Pixel[i][j] + rate_value;
 			else
-				if ((int)img.Pixel[i][j] + rate_value < 0)
+				if ((int)img->Pixel[i][j] + rate_value < 0)
 					Pixel[i][j] = 0;
 				else
-					Pixel[i][j] = img.Pixel[i][j] + rate_value;
+					Pixel[i][j] = img->Pixel[i][j] + rate_value;
 		}
 	}
 
 }
-int Image::contrastCtrl(Image img)
+int Image::contrastCtrl(Image *img)
 {
 
 }
@@ -182,7 +197,7 @@ void Image::pixelWrite(char file_name[])
 {
 	ofstream outFile;
 	outFile.open(file_name, ios::out | ios::app);
-	outFile.write((char *)&Pixel, height*width);
+	outFile.write((char *)Pixel, height*width);
 	outFile.close();
 }
 #endif
